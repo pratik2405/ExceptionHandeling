948f7a67f0c348e725ae9cf2fb0d156751dfc481
pratik2405 <shinde7388pratik@gmail.com>
2024-11-13 15:43:55 +0530

    Add project files.

---
+++ ExceptionHandeling/AgeNameCustomExcep.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandeling
+{
+    public class AgeCustomExcep:Exception
+    {
+        public AgeCustomExcep(string msg):base(msg)
+        { 
+        }
+    }
+
+    public class NameCustomExcep : Exception
+    {
+        public NameCustomExcep(string msg) : base(msg)
+        {
+        }
+    }
+
+    public class User
+    {
+        private string name;
+        private int age;
+
+        public User(int age, string name)
+        {
+            if (age >= 18)
+            {
+                this.age = age;
+            }
+            else
+            {
+                throw new AgeCustomExcep(age + " age is not allowed");
+            }
+
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new NameCustomExcep("Null not accepted !");
+            }
+            else if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new NameCustomExcep("Name Empty is allowed !");
+            }
+            else
+            {
+                this.name = name;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Age= {age} , Name={name}";
+        }
+    }
+}
---
+++ ExceptionHandeling/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandeling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                User user = new User(19, " ");
+                Console.WriteLine(user);
+            }
+            catch(AgeCustomExcep a)
+            {
+                Console.WriteLine(a.Message);
+            }
+            //try
+            //{
+            //    User user = new User(19, " ");
+            //}
+            catch (NameCustomExcep c)
+            {
+                Console.WriteLine(c.Message);
+            }
+            catch (Exception e) 
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
